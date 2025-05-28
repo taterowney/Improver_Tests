763d94eed62765acacb074c9be44b34025ffe13b
taterowney <51540870+taterowney@users.noreply.github.com>
2025-05-28 10:13:05 -0400

    Running on cluster

--- agent.py
+++ agent.py
@@ -51,10 +51,11 @@ def score_proof(proof):
             futures.append(executor.submit(client.chat.completions.create,
                     model=MODEL_NAME,
                     messages=messages,
-                    seed=42,
                     temperature=0.0,
+                    top_p=1.0,
                     extra_body={
-                        "top_p": 1.0,
+                        "seed": 42,
+                        "top_k": 1,
                     }
                 )
             )
@@ -119,10 +120,10 @@ theorem kernel_is_normal_subgroup (h : α → β) (is_hom : is_homomorphism h) :
       exact this
     exact { conj_mem := conj_mem }
   rfl"""
-    # print(score_proof(test_proof), score_proof(test_proof), score_proof(test_proof))
+    print(score_proof(test_proof), score_proof(test_proof), score_proof(test_proof))
 
 
-    model = load_offline_model()
-    print(score_proof_offline(test_proof, model))
-    print(score_proof_offline(test_proof, model))
-    print(score_proof_offline(test_proof, model))
+    # model = load_offline_model()
+    # print(score_proof_offline(test_proof, model))
+    # print(score_proof_offline(test_proof, model))
+    # print(score_proof_offline(test_proof, model))
